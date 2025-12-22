21aed2182b9a8c03723e024a1d80617ceb1f284b
unknown <coded8125@gmail.com>
2025-12-22 20:56:30 +0530

    array_list

---
+++ src/main/java/org/example/module_5/list/sorting_array_list.java
@@ -0,0 +1,32 @@
+package org.example.module_5.list;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class sorting_array_list {
+    ArrayList<Integer> myArr1=new ArrayList<>();
+
+    public static void main(String[] args) {
+        sorting_array_list obj = new sorting_array_list();
+        obj.addElement();
+        System.out.println("original array");
+        obj.printArray();
+        System.out.println("sorting the array in ascending order");
+        Collections.sort(obj.myArr1);
+        obj.printArray();
+    }
+    public void addElement(){
+        myArr1.add(5);
+        myArr1.add(2);
+        myArr1.add(8);
+        myArr1.add(1);
+        myArr1.add(4);
+    }
+    public void printArray(){
+        System.out.println("printing array element");
+        for(Integer num: myArr1){
+            System.out.println(num +" ");
+        }
+        System.out.println();
+    }
+}
